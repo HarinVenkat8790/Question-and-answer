2933cd3ae301a992819fb4424f65319bc89d0c94
PogChamp <95856144+HarinVenkat8790@users.noreply.github.com>
2021-12-26 20:09:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //this is to get the window title
+            Console.Title = "quiz";
+            
+            //the question
+            Console.WriteLine("What is the best cartoon trilogy in the world");
+            string name = Console.ReadLine();
+
+            if (name == "Oggy and the cockroaches")
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+                Console.ReadLine();
+
+                Console.WriteLine("You Have Other Chance"); 
+                Console.ReadLine();
+                  
+                Console.WriteLine("Last chance");
+                Console.ReadLine();
+  
+                Environment.Exit(1);
+                
+            }    
+
+            
+            
+            
+            Console.WriteLine("Who is the best human in the world");
+            string answer = Console.ReadLine();
+
+            
+            if (answer == "elon musk")
+            {
+                Console.WriteLine("Correct");
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+                Console.ReadLine();
+
+                Console.WriteLine("You Have Other Chance"); 
+                Console.ReadLine();
+                  
+                Console.WriteLine("Last chance");
+                Console.ReadLine();
+
+            }
+                
+                Console.ReadKey();
+        }
+    }
+}
